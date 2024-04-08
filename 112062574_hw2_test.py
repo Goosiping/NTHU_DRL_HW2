1fc28f1c16d33300fb4d474449e49a4965e1aaec
郭士平 <georgekuo100@gmail.com>
2024-04-09 04:54:01 +0800

    reload 18

--- 112062574_hw2_test.py
+++ 112062574_hw2_test.py
@@ -1,14 +1,11 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import cv2
 
 import gym
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
-
-SHAPE = (1, 84, 84)
 
 '''
 class QNetwork(nn.Module):
@@ -44,7 +41,7 @@ class QNetwork(nn.Module):
         out = self.fc(conv_out)
         return out
 '''
-'''
+
 class QNetwork(nn.Module):
     """
     For Gray Scale Image
@@ -77,40 +74,6 @@ class QNetwork(nn.Module):
         conv_out = self.conv(x)
         out = self.fc(conv_out)
         return out
-'''    
-class QNetwork(nn.Module):
-    def __init__(self, n_actions):
-        super(QNetwork, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(SHAPE[0], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 64, kernel_size=3, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 32, kernel_size=3, stride=2),
-            nn.ReLU(),
-        )
-
-        conv_out_size = self._get_conv_out(SHAPE)
-
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(conv_out_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, n_actions),
-        )  
-
-    def _get_conv_out(self, shape):
-        o = self.conv(torch.zeros(1, *shape))
-        return int(np.prod(o.size()))
-
-    def forward(self, x):
-        conv_out = self.conv(x)
-        out = self.fc(conv_out)
-        return out
 
 class Agent(object):
     """Agent that acts randomly."""
@@ -119,29 +82,19 @@ class Agent(object):
         self.q = QNetwork(self.action_space.n)
         self.q.load_state_dict(torch.load('./112062574_hw2_data', map_location=torch.device('cpu')))
         print("INFO: Model loaded successfully.")
-
-        self.frame_id = 0
-        self.frame_skip = 4
+        
 
     def act(self, observation):
-        if self.frame_id % self.frame_skip == 0:
-            observation = self._preprocess(observation)
-            observation = torch.Tensor(observation.copy()).unsqueeze(0)
-            with torch.no_grad():
-                action = self.q(observation).argmax().item()
-                self.last_action = action
-                self.frame_id += 1
-                return action
-        else:
-            self.frame_id += 1
-            return self.last_action
+        observation = self._preprocess(observation)
+        observation = torch.Tensor(observation.copy()).unsqueeze(0)
+        with torch.no_grad():
+            return self.q(observation).argmax().item()
         
     def _preprocess(self, state):
-        state = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
-        state = cv2.resize(state, (SHAPE[1], SHAPE[2]), interpolation=cv2.INTER_AREA)
+        state = np.dot(state[...,:3], [0.299, 0.587, 0.114])
         state = state.astype(np.float32) / 255.0
-        state = np.reshape(state, SHAPE)
-        return state   
+        state = np.reshape(state, (1, 240, 256)) 
+        return state       
 
 
 if __name__ == "__main__":
@@ -158,8 +111,6 @@ if __name__ == "__main__":
         done = False
         while not done:
             action = agent.act(state)
-
-            
             state, reward, done, info = env.step(action)
             env.render()
             score += reward
